23b248e4d05dc444c22b311eddf0fb2548113dc9
Paulo Carneiro <paulocarneirosegundo@gmail.com>
2023-09-01 11:18:03 -0300

    Adding functions

---
+++ TICTACTOE.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <ctime>
+//TIC TAC TOE
+
+void drawBoard(char* spaces);
+void playerMove(char* spaces, char player);
+void computerMove(char* spaces, char computer);
+bool checkWinner(char* spaces, char player, char computer);
+bool checkTie(char* spaces);
+
+
+
+int main(){
+
+
+}
